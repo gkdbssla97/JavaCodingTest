484475039f3d07da26ccb0db392aafa215f9ce6e
Hayoon <hy97@sju.ac.kr>
2022-12-25 18:12:28 +0900

    Initial commit

---
+++ src/baekjoon/BOJ1406.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+class BOJ1406 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split("");
+        int M = Integer.parseInt(br.readLine());
+        LinkedList<String> linkedList = new LinkedList<>(Arrays.asList(str));
+        ListIterator<String> iter = linkedList.listIterator();
+
+        while (iter.hasNext()) {
+            iter.next();
+            System.out.println(1);
+        }
+        System.out.println(2);
+        for (int i = 0; i < M; i++) {
+            String[] cmd = br.readLine().split(" ");
+            if (cmd[0].equals("P")) {
+                iter.add(cmd[1]);
+            } else if (cmd[0].equals("L")) {
+                if (iter.hasPrevious())
+                    iter.previous();
+            }else if (cmd[0].equals("D")) {
+                if (iter.hasNext())
+                    iter.next();
+            }else if (cmd[0].equals("B")) {
+                if (iter.hasPrevious()) {
+                    iter.previous();
+                    iter.remove();
+                }
+            }
+        }
+        System.out.println(String.join("",linkedList));
+    }
+}
---
+++ src/syntax/PracticeInput.java
@@ -0,0 +1,21 @@
+package syntax;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class PracticeInput {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+
+        StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
+        System.out.println(stringTokenizer.nextToken());
+        System.out.println(stringTokenizer.nextToken());
+        System.out.println(stringTokenizer.nextToken());
+        System.out.println(Arrays.toString(s));
+    }
+}
