f1d8a90bb710bcecac3de982fbcb2c7a070ddf7e
Hayoon <hy97@sju.ac.kr>
2022-12-25 18:31:55 +0900

    How to use the BufferReader in Java InputStreamReader

---
+++ src/baekjoon/in_output/BOJ2558.java
@@ -0,0 +1,16 @@
+package baekjoon.in_output;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2558 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a + b);
+
+    }
+}
