8923288445e25b713daeb3b6305e113406a9d3cb
Hayoon <hy97@sju.ac.kr>
2022-12-25 18:44:39 +0900

    just same as previous problem

---
+++ src/baekjoon/in_output/BOJ10951.java
@@ -0,0 +1,20 @@
+package baekjoon.in_output;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ10951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int T = Integer.parseInt(br.readLine());
+        String str;
+        while((str = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(str, " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            System.out.println(a + b);
+        }
+    }
+}
