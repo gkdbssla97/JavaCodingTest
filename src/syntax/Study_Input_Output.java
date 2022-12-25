9814e82f421724c095d55c18bcf7ab234f383a39
Hayoon <hy97@sju.ac.kr>
2022-12-25 19:29:04 +0900

    .

--- src/syntax/Study_Input_Output.java
+++ src/syntax/Study_Input_Output.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class PracticeInput {
+public class Study_Input_Output {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
