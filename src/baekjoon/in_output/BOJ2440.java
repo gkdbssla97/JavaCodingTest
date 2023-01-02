4728a63ba0089d10ae839820e250c14f8e494a71
Hayoon <hy97@sju.ac.kr>
2023-01-02 13:45:28 +0900

    marking start series

--- src/baekjoon/in_output/BOJ2440.java
+++ src/baekjoon/in_output/BOJ2440.java
@@ -5,24 +5,22 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ2439 {
+public class BOJ2440 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int a = Integer.parseInt(st.nextToken());
 
-        int space = a - 1;
-        int star = 1;
+        int space = 0;
+        int star = a;
         for (int i = 0; i < a; i++) {
-            for (int j = 0; j < space; j++) {
+            for (int k = 0; k < i; k++) {
                 System.out.print(" ");
             }
-            for (int k = 0; k < star; k++) {
+            for (int j = i + 1; j <= star; j++) {
                 System.out.print("*");
             }
             System.out.println();
-            space--;
-            star++;
         }
     }
 }
--- src/baekjoon/in_output/BOJ2441.java
+++ src/baekjoon/in_output/BOJ2441.java
@@ -5,24 +5,15 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ2439 {
+public class BOJ2441 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
 
-        int space = a - 1;
-        int star = 1;
-        for (int i = 0; i < a; i++) {
-            for (int j = 0; j < space; j++) {
-                System.out.print(" ");
-            }
-            for (int k = 0; k < star; k++) {
-                System.out.print("*");
-            }
-            System.out.println();
-            space--;
-            star++;
-        }
+        System.out.println(a + b);
+
+
     }
 }
