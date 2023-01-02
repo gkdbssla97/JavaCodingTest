97d990675077e55455fcaf10e609781f9666b7b2
Hayoon <hy97@sju.ac.kr>
2023-01-02 12:50:48 +0900

    solved. (by reading solution)

---
+++ src/baekjoon/in_output/BOJ2439.java
@@ -0,0 +1,16 @@
+package baekjoon.in_output;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ2439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int a = Integer.parseInt(st.nextToken());
+
+
+    }
+}
