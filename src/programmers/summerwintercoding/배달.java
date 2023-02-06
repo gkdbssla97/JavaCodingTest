650a8d5d88a36462e7f38e72234fc86cd1af188f
Hayoon <hy97@sju.ac.kr>
2023-02-06 15:40:35 +0900

    .solved by Dijkstra

--- src/programmers/summerwintercoding/배달.java
+++ src/programmers/summerwintercoding/배달.java
@@ -1,8 +1,15 @@
 package programmers.summerwintercoding;
 
 import java.util.ArrayList;
+import java.util.PriorityQueue;
 
 public class 배달 {
+    static int N;
+    static int K;
+    static ArrayList<ArrayList<Node>> graph;
+    static int[] dist;
+    static boolean[] visited;
+
     static class Node {
         int idx;
         int cost;
@@ -13,56 +20,52 @@ public class 배달 {
         }
     }
 
-    static ArrayList<ArrayList<Node>> graph;
-    static boolean[] visited;
-    static int[] dist;
     public static int solution(int N, int[][] road, int K) {
+        int answer = 0;
         graph = new ArrayList<ArrayList<Node>>();
+        dist = new int[N + 1];
+        visited = new boolean[N + 1];
 
         for (int i = 0; i < N + 1; i++) {
             graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < N + 1; i++) {
+            dist[i] = Integer.MAX_VALUE;
         }
         for (int i = 0; i < road.length; i++) {
             graph.get(road[i][0]).add(new Node(road[i][1], road[i][2]));
             graph.get(road[i][1]).add(new Node(road[i][0], road[i][2]));
         }
-        visited = new boolean[N + 1];
-        dist = new int[N + 1];
-        for (int i = 0; i < N + 1; i++) {
-            dist[i] = Integer.MAX_VALUE;
-        }
+        PriorityQueue<Node> queue = new PriorityQueue<>
+                ((o1, o2) -> Integer.compare(o1.cost, o2.cost));
+        queue.offer(new Node(1, 0));
         dist[1] = 0;
-        for (int i = 0; i < N; i++) {
-            int nodeValue = Integer.MAX_VALUE;
-            int nodeIdx = 0;
-            for (int j = 1; j < N + 1; j++) {
-                if (!visited[j] && dist[j] < nodeValue) {
-                    nodeValue = dist[j];
-                    nodeIdx = j;
-                }
-            }
-            visited[nodeIdx] = true;
-            for (int j = 0; j < graph.get(nodeIdx).size(); j++) {
-                Node adjNode = graph.get(nodeIdx).get(j);
-                if (dist[adjNode.idx] > dist[nodeIdx] + adjNode.cost) {
-                    dist[adjNode.idx] = dist[nodeIdx] + adjNode.cost;
+        while (!queue.isEmpty()) {
+            Node poll = queue.poll();
+            if (dist[poll.idx] < poll.cost)
+                continue;
+            for (int i = 0; i < graph.get(poll.idx).size(); i++) {
+                Node nextNode = graph.get(poll.idx).get(i);
+                if (dist[nextNode.idx] > poll.cost + nextNode.cost) {
+                    dist[nextNode.idx] = poll.cost + nextNode.cost;
+                    queue.offer(new Node(nextNode.idx, dist[nextNode.idx]));
                 }
             }
         }
-        int answer = 0;
-        for (int i = 1; i < N + 1; i++) {
-            if (dist[i]<= K) answer++;
-        }
 
-
+//        for (int x : dist) {
+//            if (x <= K)
+//                answer++;
+//        }
+//        System.out.println(answer);
         return answer;
     }
 
     public static void main(String[] args) {
-
-        int N = 5;
+        N = 5;
         int[][] road = {{1, 2, 1}, {2, 3, 3}, {5, 2, 2}, {1, 4, 2}, {5, 3, 1}, {5, 4, 2}};
-        int K = 3;
+        K = 3;
+
         solution(N, road, K);
     }
 }
