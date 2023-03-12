983999a3fffdac7f829f7752b6a0e69557b62b9d
Hayoon <hy97@sju.ac.kr>
2023-03-12 15:35:52 +0900

    AC (solved TC8, TC18)

--- src/programmers/kakao/BLINDRECRUITMENT2022/양궁대회.java
+++ src/programmers/kakao/BLINDRECRUITMENT2022/양궁대회.java
@@ -1,8 +1,6 @@
 package programmers.kakao.BLINDRECRUITMENT2022;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class 양궁대회 {
     static int[] score = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
@@ -10,7 +8,6 @@ public class 양궁대회 {
     static int[] res_ryan = new int[11];
 
     public static int[] solution(int n, int[] info) {
-        int[] answer = {};
         int[] res = new int[n];
 
         combination_with_repetition(info, n, res, 0, 0);
@@ -20,18 +17,11 @@ public class 양궁대회 {
 
     static void combination_with_repetition(int[] info, int r, int[] tmp, int cur, int start) {
         if (r == cur) {
-            System.out.println(Arrays.toString(tmp));
             int[] copy_info = info.clone();
             int[] ryan_info = new int[11];
             for (int e : tmp) {
                 ryan_info[10 - e] += 1;
             }
-            boolean flag = false;
-//            if (tmp[0] == 9 && tmp[1] == 9 && tmp[2] == 8 && tmp[3] == 8 && tmp[4] == 6) {
-//                System.out.println(Arrays.toString(copy_info));
-//                System.out.println(Arrays.toString(ryan_info));
-//                flag = true;
-//            }
 
             int ryan = 0;
             int apeach = 0;
@@ -43,15 +33,19 @@ public class 양궁대회 {
                     } else ryan += 10 - i;
                 }
             }
-//            if (flag) System.out.println(apeach + " " + ryan);
+
+            boolean isLarge = false;
             if (res_max == ryan - apeach) {
                 for (int i = 10; i >= 0; i--) {
-                    if (ryan_info[i] > res_ryan[i]) {
-                        res_ryan = ryan_info.clone();
-                    }
+                    if (ryan_info[i] == res_ryan[i])
+                        continue;
+                    else if (ryan_info[i] > res_ryan[i]) {
+                        isLarge = true;
+                        break;
+                    } else break;
                 }
-            }
-            if (res_max < ryan - apeach) {
+                if(isLarge) res_ryan = ryan_info.clone();
+            } else if (res_max < ryan - apeach) {
                 res_max = ryan - apeach;
                 res_ryan = ryan_info.clone();
             }
@@ -70,8 +64,3 @@ public class 양궁대회 {
         System.out.println(Arrays.toString(res_ryan));
     }
 }
-//23
-// 9: 2 8: 2 6: 1
-// 9: 2 6: 1 5: 1 4: 1
-//[0, 2, 0, 1, 1, 1, 0, 0, 0, ...]
-//24
