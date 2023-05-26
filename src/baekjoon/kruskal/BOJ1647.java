1e0a2ff7f821d592dc9f74014bfb226c0c94c140
Hayoon <hy97@sju.ac.kr>
2023-05-26 19:11:44 +0900

    solved by using Kruskal Algorithm

--- src/baekjoon/kruskal/BOJ1647.java
+++ src/baekjoon/kruskal/BOJ1647.java
@@ -5,58 +5,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class BOJ1922 {
+public class BOJ1647 {
     static int N, M;
-    static int V, E;
     static int[][] board;
     static int[] parent;
     static int final_cost;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] a = br.readLine().split(" ");
-        N = Integer.parseInt(a[0]);
-        M = Integer.parseInt(a[1]);
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
 
         board = new int[M][3];
         parent = new int[N + 1];
+        final_cost = 0;
 
         for (int i = 1; i <= N; i++) {
             parent[i] = i;
         }
-        for (int i = 0; i < M; i++) {
-            String[] s = br.readLine().split(" ");
-            board[i][0] = Integer.parseInt(s[0]);
-            board[i][1] = Integer.parseInt(s[1]);
-            board[i][2] = Integer.parseInt(s[2]);
-        }
 
         Arrays.sort(board, (o1, o2) -> Integer.compare(o1[2], o2[2]));
-        int big_cost = 0;
+
         for (int i = 0; i < M; i++) {
-            if (find(board[i][0]) != find(board[i][1])) {
+            String[] ss = br.readLine().split(" ");
+            board[i][0] = Integer.parseInt(ss[0]);
+            board[i][1] = Integer.parseInt(ss[1]);
+            board[i][2] = Integer.parseInt(ss[2]);
+        }
+
+        for (int i = 0; i < M; i++) {
+            if(find(board[i][0]) != find(board[i][1])) {
                 union(board[i][0], board[i][1]);
                 final_cost += board[i][2];
-                big_cost = board[i][2];
+                continue;
             }
         }
-        System.out.println(final_cost - big_cost);
+        System.out.println(final_cost);
+    }
+    static int find(int a) {
+        if(parent[a] == a) {
+            return parent[a];
+        }
+        return find(parent[a]);
     }
 
     static void union(int a, int b) {
         a = find(a);
         b = find(b);
-        if (a > b) {
+
+        if(a > b) {
             parent[a] = b;
         } else {
             parent[b] = a;
         }
-    }
-
-    static int find(int a) {
-        if (parent[a] == a) {
-            return parent[a];
-        }
-        return find(parent[a]);
     }
 }
