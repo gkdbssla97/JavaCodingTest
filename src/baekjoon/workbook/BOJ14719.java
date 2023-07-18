bd535ef074bb9d7ae1557974a391e06cc4bff96c
Hayoon <hy97@sju.ac.kr>
2023-07-18 23:05:58 +0900

    brute force searching two ways, left and right

--- src/baekjoon/workbook/BOJ14719.java
+++ src/baekjoon/workbook/BOJ14719.java
@@ -8,56 +8,40 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class BOJ14719 {
-    static int H, W;
     static int[][] board;
-    static boolean[][] visited;
-    static int[] rain;
-    static int res = 0;
+    static int left, right;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
+        int h = Integer.parseInt(s[0]);
+        int w = Integer.parseInt(s[1]);
 
-        H = Integer.parseInt(s[0]);
-        W = Integer.parseInt(s[1]);
-
-        rain = new int[W];
-        board = new int[H][W];
-        visited = new boolean[H][W];
-
+        board = new int[h][w];
+        int res = 0;
         String[] ss = br.readLine().split(" ");
+        int[] height = new int[ss.length];
         for (int i = 0; i < ss.length; i++) {
-            rain[i] = Integer.parseInt(ss[i]);
+            height[i] = Integer.parseInt(ss[i]);
         }
+        for(int i = 1; i < height.length - 1; i++) {
+            //왼쪽 가장 높은거
+            int _l = 0;
+            for(int left = 0; left < i; left++) {
+                _l = Math.max(height[left], _l);
+            }
+            //오른쪽 가장 높은거
+            int _r = 0;
+            for(int right = i + 1; right < height.length; right++) {
+                _r = Math.max(height[right], _r);
+            }
 
-        for(int i = 1; i < W - 1; i++) {
-            int current = rain[i];
-            int leftMax = rain[i];
-            int rightMax = rain[i];
-            for(int j = i - 1; j >= 0; j--) {
-                if(rain[j] > current) {
-                    leftMax = Math.max(leftMax, rain[j]);
-                }
+            int _res = Math.min(_l, _r);
+            if(_res - height[i] > 0) {
+                res += _res - height[i];
             }
-            for(int j = i + 1; j < W; j++) {
-                if(rain[j] > current) {
-                    rightMax = Math.max(rightMax, rain[j]);
-                }
-            }
-            int val = Math.min(leftMax, rightMax);
-            res += val - current;
+//            System.out.println(_res - height[i]);
         }
         System.out.println(res);
-//        for (int i = 0; i < H; i++) {
-//            System.out.println(Arrays.toString(board[i]));
-//        }
-    }
-
-    static void stackRain(int col, int num) {
-        int tmp = H;
-        for (int i = 0; i < num; i++) {
-            board[tmp - 1][col] = 1;
-            tmp--;
-        }
     }
 }
