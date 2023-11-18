e49f42a6c4be19f3696723ccfb995de67d3df583
Hayoon <hy97@sju.ac.kr>
2023-11-18 21:54:31 +0900

    solved by using greedy algorithm

--- src/syntax/syntax_test.java
+++ src/syntax/syntax_test.java
@@ -1,29 +1,38 @@
 package syntax;
-
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.io.*;
 
 public class syntax_test {
+
+    static int[] parent;
+
     public static void main(String[] args) {
-
-        int r = 5;
-        int c = 5;
-        int[][] arr = {
-                {1, 2, 3, 4, 5},
-                {1, 2, 3, 4, 5},
-                {1, 2, 3, 4, 5},
-                {1, 2, 3, 4, 5},
-                {1, 2, 3, 4, 5}
-        };
-        int[][] prefix = new int[r + 1][c + 1];
-        for (int i = 1; i < r + 1; i++) {
-            for (int j = 1; j < c + 1; j++) {
-                prefix[i][j] = prefix[i -1][j] + prefix[i][j-1] - prefix[i-1][j-1] + arr[i-1][j-1];
-            }
+        int n = 3; // 노드의 개수
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i; // 각 노드의 부모 노드를 자신으로 초기화
         }
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println(Arrays.toString(prefix[i]));
+        union(0, 2);
+        union(1, 2);
+
+        if (find(0) == find(1)) {
+            System.out.println("노드 0과 노드 1은 같은 신장 트리에 속해있습니다.");
+        } else {
+            System.out.println("노드 0과 노드 1은 다른 신장 트리에 속해있습니다.");
         }
+        System.out.println(Arrays.toString(parent));
+    }
+
+    static void union(int a, int b) {
+        a = find(a);
+        b = find(b);
+        if(a < b) parent[b] = a;
+        else parent[a] = b;
+    }
+
+    static int find(int a) {
+        if(parent[a] == a) return a;
+        return parent[a] = find(parent[a]);
     }
 }
