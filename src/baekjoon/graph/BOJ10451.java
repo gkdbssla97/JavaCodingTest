15c11a8dfef2234ec4e54968e321edb20f968d77
Hayoon <hy97@sju.ac.kr>
2024-02-26 20:49:56 +0900

    solved by cycle graph with dfs

--- src/baekjoon/graph/BOJ10451.java
+++ src/baekjoon/graph/BOJ10451.java
@@ -3,51 +3,44 @@ package baekjoon.graph;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 import java.util.stream.Collectors;
 
 public class BOJ10451 {
-
     static int[][] board;
+    static int cnt;
     static boolean[] visited;
-    static int N;
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(br.readLine());
+        for(int t = 0; t < T; t++) {
+            int n = Integer.parseInt(br.readLine());
 
-        int T = Integer.parseInt(st.nextToken());
-        for (int i = 0; i < T; i++) {
-            int cnt = 0;
-            N = Integer.parseInt(br.readLine());
-            board = new int[N + 1][N + 1];
-            visited = new boolean[N + 1];
-            List<String> collect = Arrays.stream(br.readLine().split(" "))
-                    .collect(Collectors.toList());
-            List<Integer> array = collect.stream()
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-            for (int j = 0; j < N; j++) {
-                board[j + 1][array.get(j)] = 1;
-                board[array.get(j)][j + 1] = 1;
+            String[] s = br.readLine().split(" ");
+            board = new int[n + 1][n + 1];
+            for(int i = 0; i < n; i++) {
+                board[i + 1][Integer.parseInt(s[i])] = 1;
             }
-            for (int k = 1; k <= N; k++) {
-                if (!visited[k]) {
-                    dfs(k);
+            cnt = 0;
+            visited = new boolean[n + 1];
+            for(int i = 1; i <= n; i++) {
+                if(!visited[i]) {
+                    visited[i] = true;
+                    dfs(i, n);
                     cnt++;
                 }
             }
             System.out.println(cnt);
         }
     }
-
-    static void dfs(int i) {
-        visited[i] = true;
-        for (int x = 1; x <= N; x++) {
-            if (!visited[x] && board[i][x] == 1) {
-                visited[x] = true;
-                dfs(x);
+    static void dfs(int start, int n) {
+        for(int i = 1; i <= n; i++) {
+            if(!visited[i] && board[start][i] == 1) {
+                visited[i] = true;
+                dfs(i, n);
             }
         }
     }
