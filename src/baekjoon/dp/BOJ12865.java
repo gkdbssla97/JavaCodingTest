5811a8b815148af6659d1e08efaa5c36a827c1be
Hayoon <hy97@sju.ac.kr>
2024-04-26 02:45:38 +0900

    solved by two dimension dynamic programming

--- src/baekjoon/dp/BOJ12865.java
+++ src/baekjoon/dp/BOJ12865.java
@@ -7,28 +7,30 @@ import java.util.Arrays;
 public class BOJ12865 {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         String[] s = br.readLine().split(" ");
         int n = Integer.parseInt(s[0]);
         int k = Integer.parseInt(s[1]);
+
+        int[][] board = new int[n + 1][2];
         int[] dp = new int[k + 1];
-        int[] w = new int[n + 1];
-        int[] v = new int[n + 1];
-        for (int i = 1; i <= n; i++) {
-            String[] ss = br.readLine().split(" ");
-            w[i] = Integer.parseInt(ss[0]);
-            v[i] = Integer.parseInt(ss[1]);
+        for(int i = 0; i < n; i++) {
+            s = br.readLine().split(" ");
+            int w = Integer.parseInt(s[0]);
+            int v = Integer.parseInt(s[1]);
+
+            board[i + 1][0] = w;
+            board[i + 1][1] = v;
         }
-        for(int i = 1; i <= n; i++) {
+
+        for(int i = 0; i <= n; i++) {
             for(int j = k; j >= 0; j--) {
-                if(j - w[i] >= 0) {
-                    dp[j] = Math.max(dp[j], dp[j - w[i]] + v[i]);
+                if(j - board[i][0] >= 0) {
+                    dp[j] = Math.max(dp[j], dp[j - board[i][0]] + board[i][1]);
                 }
             }
         }
-        int res = Integer.MIN_VALUE;
-        for(int i = 0; i <= k; i++) {
-            res = Math.max(res, dp[i]);
-        }
-        System.out.println(res);
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(dp[k]);
     }
 }
