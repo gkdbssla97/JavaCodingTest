995fc49e1bc83b6346bdafea3d3d2dce482ca9a3
Hayoon <hy97@sju.ac.kr>
2024-04-26 18:25:16 +0900

    solved by dynamic programming(LCS)

---
+++ src/baekjoon/dp/BOJ11722.java
@@ -0,0 +1,31 @@
+package baekjoon.dp;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class BOJ11722 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] board = new int[n + 1];
+        int[] dp = new int[n + 1];
+        String[] s = br.readLine().split(" ");
+        for(int i = 0; i < n; i++) {
+            board[i + 1] = Integer.parseInt(s[i]);
+        }
+        Arrays.fill(dp, 1);
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j < i; j++) {
+                if(board[j] > board[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+//        System.out.println(Arrays.toString(dp));
+        System.out.println(Arrays.stream(dp).map(e -> e).max().getAsInt());
+
+    }
+}
