176f798d664ba99877b7e38921bc75282950d66c
Hayoon <hy97@sju.ac.kr>
2025-03-23 17:02:54 +0900

    [Gold V] Title: 센서, Time: 180 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Gold/2212. 센서/센서.java
@@ -0,0 +1,27 @@
+//package baekjoon.graph;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        String[] s = br.readLine().split(" ");
+        int[] array = Arrays.stream(s).mapToInt(Integer::parseInt).sorted().toArray();
+        int[] gap = new int[array.length - 1];
+        for(int i = 1; i < array.length; i++) {
+            gap[i - 1] = array[i] - array[i - 1];
+        }
+//        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+//        System.out.println();
+        gap = Arrays.stream(gap).sorted().toArray();
+        int sum = 0;
+        for(int i = 0; i < a - b; i++) {
+            sum += gap[i];
+        }
+        System.out.println(sum);
+    }
+}
