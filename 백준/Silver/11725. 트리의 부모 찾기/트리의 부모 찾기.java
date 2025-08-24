9f68352ad25edd9908545039838bbfcb17dd90d3
Hayoon <hy97@sju.ac.kr>
2025-08-25 01:11:34 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1188 ms, Memory: 88380 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -1,54 +1,64 @@
-//package baekjoon.graph;
+// package syntax;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
 
 public class Main {
-    static ArrayList<ArrayList<Integer>> arr;
-    static int[] visited;
-    static int[] res;
-    static int N;
+    static int res;
+    static List<ArrayList<Integer>> arr;
+    static boolean[] visited;
+    static int[] parent;
 
-    static void bfs() {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
-        visited[1] = 1;
-        while (!q.isEmpty()) {
-            Integer poll = q.poll();
-            for (Integer i : arr.get(poll)) {
-                if (visited[i] == 0) {
-                    visited[i] = 1;
-                    res[i] = poll;
-                    q.offer(i);
-                }
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine());
         arr = new ArrayList<>();
-        visited = new int[N + 1];
-        res = new int[N + 1];
-
-        for (int i = 0; i <= N; i++) {
+        for (int i = 0; i <= n; i++) {
             arr.add(new ArrayList<>());
         }
-        for (int i = 0; i < N - 1; i++) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
+
+        for (int i = 1; i < n; i++) {
+            String[] s = br.readLine().split(" ");
+            int a = Integer.parseInt(s[0]);
+            int b = Integer.parseInt(s[1]);
             arr.get(a).add(b);
             arr.get(b).add(a);
         }
-        bfs();
-        for (int i = 2; i < res.length; i++) {
-            System.out.println(res[i]);
+        
+        parent = new int[n + 1];
+        parent[1] = 0;
+        
+        visited = new boolean[n + 1];
+
+        dfs(1);
+
+        for (int i = 2; i <= n; i++) {
+            System.out.println(parent[i]);
+        }
+    }
+    
+    static void dfs(int start) {
+        for (int i = 0; i < arr.get(start).size(); i++) {
+            Integer v = arr.get(start).get(i);
+            if (!visited[v]) {
+                parent[v] = start;
+                visited[v] = true;
+                dfs(v);
+            }
         }
     }
 }
+/**
+ * 4
+ * 6
+ * 1
+ * 3
+ * 1
+ * 4
+ */
